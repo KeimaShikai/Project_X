948f423f00c05486946e78e2c95c6eb2e588a6d9
KeimaShikai <shikamaru740@gmail.com>
2018-05-27 13:22:56 +0700

    SDL added + first window

--- src/sources/main.cpp
+++ src/sources/main.cpp
@@ -1,7 +1,16 @@
 #include "main.h"
+#include <SDL2/SDL.h>
 
-int main()
+int main( int argc, char* args[] )
 {
-	test();
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+	{
+		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+	SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+	SDL_Delay(5000);
+	SDL_DestroyWindow(win);
+	SDL_Quit();
 	return 0;
 }
